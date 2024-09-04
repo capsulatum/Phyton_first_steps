92df00633108401e79699c5decb8b5cf054e74b4
Ahmet FAKI <99049127+capsulatum@users.noreply.github.com>
2024-09-04 23:17:07 +0300

    Add files via upload

--- fffirst/main.py
+++ fffirst/main.py
@@ -1,19 +1,13 @@
-mevcut_maas=int(input("mevcut maaş"))
-cocuk_sayisi=int(input("çocuk sayısı"))
-zam_miktari=int(input("zam miktarı"))
+list= []
+ort_toplam=0
+degiskensayisi = int(input("kaç adet sayı gireceksiniz"))
 
-if mevcut_maas<3000:
-    yeni_zam=mevcut_maas*20/100
+for i in range(degiskensayisi):
+    sayi=int(input("sayi giriniz"))
+    list.append(sayi)
 
-elif mevcut_maas<=4000 and mevcut_maas>=3000:
-    yeni_zam = mevcut_maas * 15 / 100
-else:
-    yeni_zam = mevcut_maas * 10 / 100
+for i in range(degiskensayisi):
+    ort_toplam+=list[i]
 
-yeni_zam=yeni_zam+(cocuk_sayisi*70)
-
-if zam_miktari>yeni_zam:
-    yeni_zam=zam_miktari
-
-print("yeni maaş miktarı {}".format(mevcut_maas+yeni_zam))
-
+ort_toplam=ort_toplam/degiskensayisi
+print(ort_toplam)
