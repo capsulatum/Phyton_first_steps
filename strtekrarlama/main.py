711a09e3e4d8d80efdb2824914b9bccfdb150c05
Ahmet FAKI <99049127+capsulatum@users.noreply.github.com>
2024-09-05 01:19:22 +0300

    Add files via upload

---
+++ strtekrarlama/main.py
@@ -0,0 +1,14 @@
+"""
+Kullanıcıdan alınan stringin
+uzunluğunun adedi
+kadar ekranda aynı stringi gösteren Python
+kodunu yazınız"""
+
+i=0
+str= input("lütfen bir kelime veya cümle giriniz")
+
+while i<len(str):
+    print(str)
+    i+=1
+
+
